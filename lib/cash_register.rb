8bfa9295dc41228873be1c06c4440956ff2d1893
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-31 18:08:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+class CashRegister 
+  attr_accessor :total, :discount, :title 
+  def initialize(employee_discount = nil)
+    @total = 0
+    @discount = employee_discount
+    @items = []
+  end 
+  def discount 
+    @discount
+  end 
+  def add_item(item, price, quantity = 1)
+    item_info = {}
+    item_info[:name] = item
+    item_info[:price] = price
+    item_info[:quantity] = quantity
 
+    @items<< item_info
+
+    @total += price * quantity
+  end 
+  def apply_discount 
+    if @discount == nil
+        "There is no discount to apply."
+    else
+        @total = @total - (@total * @discount / 100)   
+        "After the discount, the total comes to $#{@total}."
+    end
+  end 
+  def items 
+    item_names = []
+    @items.each do | item_info |
+      #test expects product name * quantity...
+      for qty in 1..item_info[:quantity] 
+        item_names << item_info[:name]
+      end 
+    end 
+    item_names
+  end 
+end
